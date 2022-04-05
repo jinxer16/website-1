be426b0571465a6f1a21345a14445bc467a19cea
PineappleSwap <pineappleswap@mail.com>
2022-04-06 00:29:59 +0930

    ETHER

--- src/components/SearchModal/CurrencySearch.tsx
+++ src/components/SearchModal/CurrencySearch.tsx
@@ -1,5 +1,6 @@
 import React, { KeyboardEvent, RefObject, useCallback, useMemo, useRef, useState, useEffect } from 'react'
-import { Currency, ETHER, Token } from '@pancakeswap/sdk'
+import { Currency, Token } from '@pancakeswap/sdk'
+import { ETHER } from './ETHER'
 import { Text, Input, Box } from '@pancakeswap/uikit'
 import { useTranslation } from 'contexts/Localization'
 import { FixedSizeList } from 'react-window'
